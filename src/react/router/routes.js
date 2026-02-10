c16b226f6127c963663e7d1887dd22a56d77296a
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 14:25:46 -0300

    Change Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,5 +1,3 @@
-import HomePage from '@controleonline/ui-manager/src/react/pages/home/index';
-import DefaultLayout from '@controleonline/ui-layout/src/react/layouts/DefaultLayout';
 import Profile from '@controleonline/ui-people/src/react/pages/Profile';
 import PurchasingSuggestion from '@controleonline/ui-orders/src/react/pages/orders/purchasing/Suggestion';
 import Inventory from '@controleonline/ui-orders/src/react/pages/inventory';
@@ -10,65 +8,54 @@ import DisplayDetails from '@controleonline/ui-ppc/src/react/pages/displays/Disp
 import DisplayForm from '@controleonline/ui-ppc/src/react/pages/displays/DisplayForm';
 import QueueAddProducts from '@controleonline/ui-ppc/src/react/pages/queues/QueueAddProducts';
 
-const WrappedComponent = (Component) => ({ navigation, route }) => (
-  <DefaultLayout navigation={navigation} route={route}>
-    <Component navigation={navigation} route={route} />
-  </DefaultLayout>
-);
-
 const managerRoutes = [
   {
-    name: 'HomePage',
-    component: WrappedComponent(HomePage),
-    options: { headerShown: false, title: 'Menu' },
-  },
-  {
     name: 'DisplayList',
-    component: WrappedComponent(DisplayList),
+    component: DisplayList,
     options: { headerShown: false, title: 'Displays' },
   },
   {
     name: 'DisplayDetails',
-    component: WrappedComponent(DisplayDetails),
+    component: DisplayDetails,
     options: { headerShown: false, title: 'Detalhes do Display' },
   },
   {
     name: 'DisplayForm',
-    component: WrappedComponent(DisplayForm),
+    component: DisplayForm,
     options: { headerShown: false, title: 'Formulário de Display' },
   },
   {
     name: 'QueueAddProducts',
-    component: WrappedComponent(QueueAddProducts),
+    component: QueueAddProducts,
     options: { headerShown: true, title: 'Produtos da Fila' },
   },
   {
     name: 'CashRegistersIndex',
-    component: WrappedComponent(CashRegisters),
+    component: CashRegisters,
     options: { headerShown: false, title: 'Caixas', headerBackButtonMenuEnabled: false },
     initialParams: { store: 'invoice' },
   },
   {
     name: 'ProfilePage',
-    component: WrappedComponent(Profile),
+    component: Profile,
     options: { headerShown: false, title: 'Perfil', headerBackButtonMenuEnabled: false },
     initialParams: { store: 'auth' },
   },
   {
     name: 'IncomeStatment',
-    component: WrappedComponent(IncomeStatment),
+    component: IncomeStatment,
     options: { headerShown: true, title: 'Faturamento', headerBackButtonMenuEnabled: false },
     initialParams: { store: 'invoice' },
   },
   {
     name: 'PurchasingSuggestion',
-    component: WrappedComponent(PurchasingSuggestion),
+    component: PurchasingSuggestion,
     options: { headerShown: true, title: 'Sugestão de Compras', headerBackButtonMenuEnabled: false },
     initialParams: { store: 'products' },
   },
   {
     name: 'Inventory',
-    component: WrappedComponent(Inventory),
+    component: Inventory,
     options: { headerShown: true, title: 'Estoque', headerBackButtonMenuEnabled: false },
     initialParams: { store: 'products' },
   },
